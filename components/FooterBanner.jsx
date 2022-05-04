4da297252b32ad850c92dafffe6203cc361c0af3
william <william@williams-MacBook-Pro.local>
2022-05-04 15:10:35 +0100

    color2

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -3,53 +3,87 @@ import Link from 'next/link'
 import { defaultHead } from 'next/head'
 //import {urlFor} from '../lib/client'
 
-function FooterBanner({data,hourOnly,dayInfo,}) {
-
+function FooterBanner({ data, hourOnly, dayInfo }) {
   function getOrdinalNum(n) {
-    return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
+    return (
+      n +
+      (n > 0
+        ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10]
+        : '')
+    )
   }
- 
+
   //console.log("Day",new Date().getDay())
 
-  let colorh = String(dayInfo[0]?.colorId || 'pink-500')
- 
+  // let colorh = String(dayInfo[0]?.colorId || 'pink')
+
+  //let colors = ["red","green","purple","yellow","pink","orange","blue"]
+
+  //let colorRand = colors[Math.floor(Math.random()*colors.length)]
+
   //console.log(dayInfo)
   return (
-    <div className={`py-4 px-6 bg-pink-500 text-lg rounded-2xl relative  flex flex-col h leading- w-4/5 text-white mt-8 mb-12 overflow-hidden`}>
-     <div className=" flex flex-col items-center ">
+    <div
+      className={`py-4 px-6 bg-${dayInfo[0]?.color}-500 text-lg rounded-2xl relative  flex flex-col h leading- w-4/5 text-white mt-8 mb-12 overflow-hidden`}
+    >
+      <div className=" flex flex-col items-center ">
+        <div className=" flex flex-col items-center ">
+          <small className="font-cinzel text-lg">
+            {getOrdinalNum(hourOnly) == 0 ? '24th' : getOrdinalNum(hourOnly)}{' '}
+            Hour
+          </small>
+          <img className="w-32 rounded-2xl h-32 " src={data[0]?.image} />
+          <h3 className="font-bold text-2xl font-poppins">{data[0]?.name}</h3>
+        </div>
 
-              
-
-       <div className=" flex flex-col items-center ">
-         <small className="font-cinzel text-lg">{getOrdinalNum(hourOnly) == 0 ? "24th" : getOrdinalNum(hourOnly) } Hour</small>
-       <img className="w-32 rounded-2xl h-32 " src={data[0]?.image}/>
-       <h3 className="font-bold text-2xl font-poppins">{data[0]?.name}</h3>
-       </div>
-
-
-       <div className=" p-1 flex justify-center items-center md:max-w-3xl">
-       <p className="m-2 font-tapestry font-semibold">{data[0]?.info}</p>
-       </div>
-
-     </div>
-
-     <div className="flex flex-col justify-center  md:flex-row space-y-4 md:space-y-0 md:justify-evenly">
-
-       <div className="flex flex-col md:px-12 md:border-r-2"> 
-       <p className="text-xs text-gray-100  font-cinzel">Element : <span className="text-2xl text-gray-900 capitalize ml-2">{data[0]?.element}</span></p>
-       <p className="text-xs text-gray-100 font-cinzel ">Colors : <span className="text-2xl text-gray-900 capitalize ml-4">{data[0]?.colors}</span></p>
-       <p className="text-xs text-gray-100  font-cinzel">Planet : <span className="text-2xl text-gray-900 capitalize ml-4">{data[0]?.Planet}</span></p>
-       </div>
-            
-       <div className="flex flex-col md:border-l-2 md:px-12"> 
-       <p className="text-xs text-gray-100 font-cinzel">Today   : <span className="text-2xl text-gray-900 capitalize ml-6">{dayInfo[0]?.day}</span></p>
-       <p className="text-xs text-gray-100 font-cinzel">Colors : <span className="text-2xl text-gray-900 capitalize ml-6">{dayInfo[0]?.color}</span></p>
-       <p className="text-xs text-gray-100 font-cinzel ">Sign   : <span className="text-2xl text-gray-900 capitalize ml-8">{dayInfo[0]?.sign}</span></p>
-       </div>
-
+        <div className=" p-1 flex justify-center items-center md:max-w-3xl">
+          <p className="m-2 font-tapestry font-semibold">{data[0]?.info}</p>
+        </div>
       </div>
 
+      <div className="flex flex-col justify-center  md:flex-row space-y-4 md:space-y-0 md:justify-evenly">
+        <div className="flex flex-col md:px-12 md:border-r-2">
+          <p className="text-xs text-gray-100  font-cinzel">
+            Element :{' '}
+            <span className="text-2xl text-gray-900 capitalize ml-2">
+              {data[0]?.element}
+            </span>
+          </p>
+          <p className="text-xs text-gray-100 font-cinzel ">
+            Colors :{' '}
+            <span className="text-2xl text-gray-900 capitalize ml-4">
+              {data[0]?.colors}
+            </span>
+          </p>
+          <p className="text-xs text-gray-100  font-cinzel">
+            Planet :{' '}
+            <span className="text-2xl text-gray-900 capitalize ml-4">
+              {data[0]?.Planet}
+            </span>
+          </p>
+        </div>
 
+        <div className="flex flex-col md:border-l-2 md:px-12">
+          <p className="text-xs text-gray-100 font-cinzel">
+            Today :{' '}
+            <span className="text-2xl text-gray-900 capitalize ml-6">
+              {dayInfo[0]?.day}
+            </span>
+          </p>
+          <p className="text-xs text-gray-100 font-cinzel">
+            Colors :{' '}
+            <span className="text-2xl text-gray-900 capitalize ml-6">
+              {dayInfo[0]?.color}
+            </span>
+          </p>
+          <p className="text-xs text-gray-100 font-cinzel ">
+            Sign :{' '}
+            <span className="text-2xl text-gray-900 capitalize ml-8">
+              {dayInfo[0]?.sign}
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- weekDaysSign.js
+++ weekDaysSign.js
@@ -4,14 +4,14 @@ const weedDaysSign = [
     day:"Sunday",
     sign: 'Taurus, Libra',
     color:"red",
-    colorId:"red-500",
+    colorId:"red",
     info:"Hindus believe it is auspicious to wear red on Sunday. People also offer red flowers to Surya or the Sun God. Many keep a fast on this day eating just one meal, before sunset."
   },
   {
     day:"Monday",
     sign: 'Capricorn, Virgo',
     color:"yellow",
-    colorId:"yellow-500",
+    colorId:"yellow",
     info:"As per Hinduism, Monday’s color is yellow, though people who fast also wear white on this day. Lord Shiva is the deity associated with this day of the week so people often offer white flowers to this God. Monday is also associated with the moon and colors linked to it are silver, light gray or blue."
   },
   {
@@ -25,28 +25,28 @@ const weedDaysSign = [
     day:"Wednesday",
     sign: 'Gemini, Aquarius',
     color:"green",
-    colorId:"green-500",
+    colorId:"green",
     info:"Green is the color of the day for Wednesday. Wednesday is associated with Lord Vitthal as well as Budha (not Buddha) which is the deity linked with Mercury. Both Budha and Vitthal like green."
   },
   {
     day:"Thursday",
     sign: 'Pisces, Scorpio',
     color:"orange",
-    colorId:"orange-500",
+    colorId:"orange",
     info:"People wear orange or yellow on this day. As per Hindu culture, this day is the day of Lord Vishnu who is known to wear these colors. Orange is associated with Jupiter."
   },
   {
     day:"Friday",
     sign: 'Aries,Leo',
     color:"blue",
-    colorId:"blue-500",
+    colorId:"blue",
     info:"Fridays color is blue (sea green or aquamarine are also acceptable). It is associated with Venus. The day is also associated with Goddess Shakti who prefers white. So Hindus wear either color on these days."
   },
   {
     day:"Saturday",
     sign: 'Sagittariius',
     color:"purple",
-    colorId:"purple-500",
+    colorId:"purple",
     info:"Saturday is the color of royalty which is purple, though you can also wear black, indigo, mauve or dark grey, all of which are associated with wrath of Shani (Saturn). People in Indian villages also visit Shani shrines and make offerings of black oil, black sesame seeds and donate black clothes."
   },
 ]
